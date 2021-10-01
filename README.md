8090bb5f3506c27f5e74b09b834050c8b9e4d710
bofa1ex <bofa1exx@gmail.com>
2021-10-01 22:35:34 +0800

    ...

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # hq
-HTML parser cli using Xpath/CSS3 selector with xmltodict, it's a better piping action before jq stdin
+HTML parser using Xpath expression then dump as json,
+it's better piping action before jq stdin.
+
+## installation
+```shell
+pip install lxml
+pyinstaller -F hq.py
+sudo mv ./hq /usr/bin
+sudo chmod +x /usr/bin/hq
+```
+
+
+## usage
+
+### without special key
+input
+`hq -x '/html/body/text()' '<html><body>123123</body></html>'`
+
+output 
+`{"_0": "123"}`
+
+### with special key
+input
+`hq -x 'test=/html/body/text()` '<html><body>123123</body></html>'`
+output
+`{"test":"123"}`
+
+### with special file
+input
+`hq -x 'test=/html/body/text()' -f asd.html`
+
+output
+`{"test":"123"}`
+
+### with piping stdin
+input
+`cat asd.html | hq -x 'test=/html/body/text()'`
+
+output
+`{"test":"123"}`
+
+
+## best practice
+
+`curl http://testapi.cn | hq -x 'title=//xxxx' -x 'link=//xxxxx' | jq '[.title,.link] | transpose | map({title:.[0],link:.[1]})' > test.json`
--- hq.py
+++ hq.py
@@ -6,27 +6,27 @@ from sys import exit
 
 
 # author: bofa1ex
-# curl http://testapi.cn | hq
-# -x title="//["@id=123"]/value()"
-# -x link="//["@id=123"]/value()"
-# -x "//["@id=123"]/value()" => _0=$it,_1=$it,_2=$it
-
 def hq():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('html', nargs='?', type=argparse.FileType('rb'),
-                        default=sys.stdin, help="HTML", metavar="HTML")
+    parser = argparse.ArgumentParser(usage="hq [-x XPATH] [-f FILE] [HTML]")
+    parser.add_argument('html', nargs='?', default=sys.stdin,
+                        help="input '<html><body></body></html>'", metavar="HTML")
     parser.add_argument('-x', '--xpath', default=[], action='append',
                         help="Xpath Expression")
-    parser.add_argument('-c', '--css', default=[], action='append',
-                        help='CSS3 Selector Expression')
     parser.add_argument('-f', '--file', default='',
                         help='File to read input from')
-    args = parser.parse_args()
+    parser.add_argument('-v', '-V', '--version', action='version', version='hq v2.0')
+
+    args = parser.parse_args('<html><body></body></html>')
+
+    if len(args.xpath) == 0 and len(args.css) == 0:
+        parser.error("xpath/css expression is required")
 
     hp = etree.HTMLParser(encoding="utf-8", recover=True, strip_cdata=True)
 
-    inp = open(args.file) if args.file else args.html
-    document = etree.parse(inp, hp)
+    document = etree.fromstring(args.html, hp) if isinstance(args.html, str) else etree.parse(args.html if args.html else open(args.file))
+
+    if not document:
+        parser.error("document is none")
 
     resp = {}
     for index, exp in enumerate(args.xpath):
@@ -36,13 +36,13 @@ def hq():
         val = it[0] if len(it) == 1 else it[1]
 
         items = [
-            ele if isinstance(ele, str) else etree.tostring(ele).decode('utf-8')
+            ele.strip() if isinstance(ele, str) else etree.tostring(ele).decode('utf-8')
             for ele in list(document.xpath(val))
         ]
 
         resp[key] = items[0] if len(items) == 1 else items
 
-    sys.stdout.write(json.dumps(resp))
+    sys.stdout.writelines(json.dumps(resp))
     sys.stdout.flush()
 
 
